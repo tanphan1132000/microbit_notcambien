86c2eec2f07f024c34539129b820d23b3aed514c
tanphan1132000 <81750900+tanphan1132000@users.noreply.github.com>
2022-02-22 14:58:58 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,20 @@
-let gas_mV = 0
-let gas_raw = 0
-radio.setGroup(1)
-basic.forever(function () {
+function dht11 () {
     NPNBitKit.DHT11Read(DigitalPin.P0)
     radio.sendString("!7:TEMP:" + NPNBitKit.DHT11Temp() + "#")
     basic.pause(100)
     radio.sendString("!7:HUMI:" + NPNBitKit.DHT11Hum() + "#")
     basic.pause(30000)
+}
+function gas () {
     gas_raw = pins.analogReadPin(AnalogPin.P1)
     gas_mV = Math.map(gas_raw, 0, 1023, 0, 3300)
     radio.sendString("!23:GAS:" + gas_mV + "#")
     basic.pause(10000)
+}
+let gas_mV = 0
+let gas_raw = 0
+radio.setGroup(1)
+basic.forever(function () {
+    dht11()
+    gas()
 })
