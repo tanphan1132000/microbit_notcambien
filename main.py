50ab8dfb661e5f1914ad2d32d0ea310d057cccae
tanphan1132000 <81750900+tanphan1132000@users.noreply.github.com>
2022-03-10 09:14:53 +0700

    Update pxt.json, main.blocks, main.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+def dht11():
+    NPNBitKit.dht11_read(DigitalPin.P0)
+    radio.send_string("!7:TEMP:" + ("" + str(NPNBitKit.dht11_temp())) + "#")
+    basic.pause(100)
+    radio.send_string("!7:HUMI:" + ("" + str(NPNBitKit.dht11_hum())) + "#")
+    basic.pause(30000)
+def gas():
+    global gas_raw, gas_mV
+    gas_raw = pins.analog_read_pin(AnalogPin.P1)
+    gas_mV = Math.map(gas_raw, 0, 1023, 0, 3300)
+    radio.send_string("!23:GAS:" + ("" + str(gas_mV)) + "#")
+    basic.pause(10000)
+def IRsensor():
+    radio.send_string("!16:INFRARED:" + ("" + str(pins.digital_read_pin(DigitalPin.P2))) + "#")
+    basic.pause(1000)
+gas_mV = 0
+gas_raw = 0
 radio.set_group(1)
+led.enable(False)
 
 def on_forever():
-    NPNBitKit.dht11_read(DigitalPin.P0)
-    radio.send_string("!7:TEMP:" + str(NPNBitKit.dht11_temp()) + "#")
-    basic.pause(100)
-    radio.send_string("!7:HUMI:" + str(NPNBitKit.dht11_hum()) + "#")
-    basic.pause(30000)
+    dht11()
+    gas()
+    IRsensor()
 basic.forever(on_forever)
